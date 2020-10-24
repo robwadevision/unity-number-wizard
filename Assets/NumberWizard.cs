ebd8111a747b090162c137a7723e745e0eadc1cc
Rob Wade <rwadee14@gmail.com>
2020-10-24 17:15:54 +0100

    7th lesson

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -11,11 +11,20 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
         Debug.Log("Welcome to Number Wizard! It's time to pick a number (don't tell me!)");
         Debug.Log("The highest number you can choose is: " + max);
         Debug.Log("The lowest number you can choose is: " + min);
-        Debug.Log("Tell me if your number is higher or lower than 500");
-        Debug.Log("Up = Higher, Down = Lower, Enter = Correct");
+        Debug.Log("Got it? Ok. Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
         // print would work here but D.L is more flexible
         max = max + 1;
     }
@@ -26,18 +35,27 @@ public class NumberWizard : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Ok, higher. Got it. Is it higher or lower than " + guess + "?");
+            Debug.Log("Ok, higher. Got it.");
+            NextGuess();
+            
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Lower, alright then. Is it higher or lower than " + guess + "?");
+            Debug.Log("Lower, alright then.");
+            NextGuess();
+            
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Nice, nailed it! Thanks for playing!");
+            StartGame();
         }
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than " + guess + "?");
     }
 }
