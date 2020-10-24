b493e9fc75979b9b2e1d5c8bf881481fbb23d5cd
Rob Wade <rwadee14@gmail.com>
2020-10-24 16:23:28 +0100

    3rd lesson

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -14,12 +14,19 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("It's time to pick a number!");
         Debug.Log("The highest number you can choose is: " + max);
         Debug.Log("The lowest number you can choose is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Up = Higher, Down = Lower, Enter = Correct");
         // print would work here but D.L is more flexible
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            Debug.Log("Up was pushed");
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            Debug.Log("Down was pushed");
+        if (Input.GetKeyDown(KeyCode.Return))
+            Debug.Log("Enter was pushed");
     }
 }
