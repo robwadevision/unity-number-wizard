006d532d51a92d776347644cc3a9635c3359d2a9
Rob Wade <rwadee14@gmail.com>
2020-10-24 16:03:35 +0100

    2nd lesson

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,10 +7,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to Number Wizard");
         Debug.Log("It's time to pick a number!");
-        Debug.Log("The highest number you can choose is 1000");
-        Debug.Log("The lowest number you can choose is 1");
+        Debug.Log("The highest number you can choose is: " + max);
+        Debug.Log("The lowest number you can choose is: " + min);
         // print would work here but D.L is more flexible
     }
 
