8b066dbe586fe478a87cc15999a312040e872e40
Rob Wade <rwadee14@gmail.com>
2020-10-24 15:30:50 +0100

    Lesson 1 update

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("It's time to pick a number!");
+        Debug.Log("The highest number you can choose is 1000");
+        Debug.Log("The lowest number you can choose is 1");
+        // print would work here but D.L is more flexible
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
